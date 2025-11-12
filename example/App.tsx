551ed339a87621fc3a9fdf034d0f98c603702fe5
Joshua Ren <145518259+Angelicpancake@users.noreply.github.com>
2025-11-12 15:15:30 -0500

    2nd

--- example/App.tsx
+++ example/App.tsx
@@ -1,10 +1,34 @@
 import * as Settings from "expo-settings";
-import { Text, View } from "react-native";
+import React, { useEffect, useState } from "react";
+import { Button, Text, View } from "react-native";
 
 export default function App() {
+  const [theme, setTheme] = useState<string>(Settings.getTheme());
+  const nextTheme = theme === "light" ? "dark" : "light";
+
+  useEffect(() => {
+    const subscription = Settings.addThemeListener(({ theme: newTheme }) => {
+      setTheme(newTheme);
+    });
+    return () => subscription.remove();
+  }, [setTheme]);
+
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>Theme: {Settings.getTheme()}</Text>
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: theme === "dark" ? "#000" : "#fff",
+      }}
+    >
+      <Text style={{ color: theme === "dark" ? "#fff" : "#000" }}>
+        Theme: {theme}
+      </Text>
+      <Button
+        title="change theme"
+        onPress={() => Settings.setTheme(nextTheme)}
+      />
     </View>
   );
 }
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,29 @@
+// ExpoSettings.ts
+import { EventEmitter, EventSubscription } from "expo-modules-core";
 import ExpoSettingsModule from "./ExpoSettingsModule";
 
+export type ThemeChangeEvent = {
+  theme: string;
+};
+
+// Create an event emitter bound to the native module
+const emitter = new EventEmitter(ExpoSettingsModule);
+
+// Add listener for theme changes
+export function addThemeListener(
+  listener: (event: ThemeChangeEvent) => void
+): EventSubscription {
+  return emitter.addListener<ThemeChangeEvent>("onChangeTheme", listener);
+}
+
+// Get the current theme from native
 export function getTheme(): string {
   return ExpoSettingsModule.getTheme();
 }
+
+// Set the theme in native
+export function setTheme(theme: string): void {
+  return ExpoSettingsModule.setTheme(theme);
+}
+
+//could be deprecated
